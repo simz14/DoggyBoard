3242d573b2f3c5568393ccd0108d44a52972d645
Simona Zozuľaková <szozulakova@gmail.com>
2023-04-28 15:43:10 +0200

    router - set up

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const { sequelize } = require("./database/connections");
 
+const express = require("express");
+const router = require("./routes/routes");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 sequelize
   .authenticate()
   .then(() => {
@@ -8,3 +13,10 @@ sequelize
   .catch((error) => {
     console.error("Unable to connect to the database: ", error);
   });
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
