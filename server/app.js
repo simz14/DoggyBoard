885c134a5959e2d5e54b0483984c7ba22dd758a0
Simona Zozuľaková <szozulakova@gmail.com>
2023-04-28 15:31:55 +0200

    database set up

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const { sequelize } = require("./database/connections");
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
